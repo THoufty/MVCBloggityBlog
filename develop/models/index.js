ffa05289e10d5397ec72bee86f24aa5ca5d4ff0a
Tom <thomasjhouf@gmail.com>
2023-02-18 11:52:45 -0500

    initial commit adding all base code

---
+++ develop/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// SET UP RELATIONSHIPS
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
